fdd1ddbd5deec8b9f4c5ae1f3c360ff7b436e22b
Shubhankar Shandilya <shubhankar.shandilya.india@gmail.com>
2024-02-26 15:50:56 +0530

    Update Table.js

--- src/Component/Table.js
+++ src/Component/Table.js
@@ -44,29 +44,29 @@ const Table = () => {
             <table className='w-full flex flex-col gap-[10px]'>
                 <thead className='w-full'>
                     <tr className='flex w-full justify-around bg-[#87CEEB] rounded-md text-sm sm:text-base '>
-                        <th className='p-4 sm:w-[42%]  justify-start text-left pl-[1.7vw] font-bold text-black'>Coin</th>
-                        <th className='p-4 sm:w-[20%]  justify-start font-bold text-black'>Price</th>
-                        <th className='p-4 sm:w-[18%]  justify-start font-bold text-black'>24h Change</th>
-                        <th className='p-4 sm:w-[20%]  justify-start font-bold text-black'>Market Cap</th>
+                        <th className='p-2 sm:w-[42%]  justify-start text-left pl-[1.7vw] font-bold text-black'>Coin</th>
+                        <th className='p-2 sm:w-[20%]  justify-start font-bold text-black'>Price</th>
+                        <th className='p-2 sm:w-[18%]  justify-start font-bold text-black'>24h Change</th>
+                        <th className='p-2 sm:w-[20%]  justify-start font-bold text-black'>Market Cap</th>
                     </tr>
                 </thead>
                 <tbody className='w-full'>
                     {arr?.filter((coin)=>coin.name.toLowerCase().includes(searchquery) || coin.symbol.toLowerCase().includes(searchquery)).slice(page * 10 - 10, page * 10).map((coin) => (
                         <tr onClick={() => clicked(coin.id)} className='text-xs vs:text-base w-full flex  hover:bg-[#87CEEB] text-[#FAF0E6] hover:font-bold hover:text-black rounded-md cursor-pointer' key={coin.id}>
+ 
 
-
-                            <td className='flex items-center p-4 w-[30%] sm:w-[42%]  '>
+                            <td className='flex items-center p-2 w-[38%] sm:w-[42%]  '>
                                 <img className='h-10 pr-2 ' src={coin.image} alt="" />
                                 <div className='flex flex-col'>
                                     <button className='font-bold'>{coin.name}</button>
                                     <button className='text-xs text-left'>{coin.symbol}</button>
                                 </div>
                             </td>
-                            <td className='p-4 w-[30%] sm:w-[20%] text-center'><button>{coin.current_price.toLocaleString()}</button></td>
-                            <td className={`${getclass(coin.price_change_percentage_24h)} p-4 w-[10%] sm:w-[18%] text-center`}>
+                            <td className='p-2 w-[26%] sm:w-[20%] text-center'><button>{coin.current_price.toLocaleString()}</button></td>
+                            <td className={`${getclass(coin.price_change_percentage_24h)} p-2 w-[10%] sm:w-[18%] text-center`}>
                                 <button>{coin.price_change_percentage_24h.toFixed(2)} %</button>
                             </td>
-                            <td className='p-4 w-[30%] sm:w-[20%] text-center'><button>{(coin.market_cap / 1000000).toFixed(0).toLocaleString()} M</button></td>
+                            <td className='p-2 w-[26%] sm:w-[20%] text-center'><button>{(coin.market_cap / 1000000).toFixed(0).toLocaleString()} M</button></td>
 
 
                         </tr>
