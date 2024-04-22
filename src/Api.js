ceb3132fd29c4209236b8752e782767591f78a12
Your Name <you@example.com>
2024-04-22 19:20:51 +0530

    auth

--- src/Api.js
+++ src/Api.js
@@ -1,16 +1,4 @@
-<<<<<<< HEAD
-// export const CoinList = (currency) =>
-//   `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=100&page=1&sparkline=false`;
 
-// export const SingleCoin = (id) =>
-//   `https://api.coingecko.com/api/v3/coins/${id}`;
-
-// export const HistoricalChart = (id, days = 365, currency) =>
-//   `https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=${currency}&days=${days}`;
-
-// export const TrendingCoins = (currency) =>
-//   `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=gecko_desc&per_page=10&page=1&sparkline=false&price_change_percentage=24h`;
-=======
 export const CoinList = (currency) =>
   `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=100&page=1&sparkline=false`;
 
@@ -22,4 +10,3 @@ export const HistoricalChart = (id, days = 365, currency) =>
 
 export const TrendingCoins = (currency) =>
   `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=gecko_desc&per_page=10&page=1&sparkline=false&price_change_percentage=24h`;
->>>>>>> f1e10720203a69e7c3b0872fa5a1e6746cff20f6
